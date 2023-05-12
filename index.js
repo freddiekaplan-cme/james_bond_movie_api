d4db5386dc5e068575207095ecc95abb6045c72a
Freddie CME <freddie.kaplan@edu.cmeducations.se>
2023-05-12 14:33:50 +0200

    Update from bodyparser to app.use.

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 import express from "express"
-import bodyParser from "body-parser"
 import useRouter from "./routes/users.js"
 
 const app = express()
 
-app.use(bodyParser.json())
+app.use(express.json());
 
 app.use("/api", useRouter)
 
-app.listen(3000, function() {
+app.listen(3000, () => {
 	console.log('Listening on port 3000 😎')
 })
 
