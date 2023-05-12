36d6e4a47588cd4d45cbb40091e3b07a12ba8842
Freddie CME <freddie.kaplan@edu.cmeducations.se>
2023-05-12 14:45:49 +0200

    Add API key functionality.

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
-import express from "express"
-import useRouter from "./routes/users.js"
+import express from "express";
+import useRouter from "./routes/users.js";
 
-const app = express()
+const app = express();
 
 app.use(express.json());
 
-app.use("/api", useRouter)
+const authenticateApiKey = (req, res, next) => {
+	const apiKey = req.query.apiKey;
+	console.log(req.query);
+	
+	if (!apiKey) {
+		return res
+		.status(401)
+		.json({ message: "Inga karaktärer till dig! Din api key saknas." });
+	};
+	
+	const validApiKey = "5";
+	
+	if (apiKey !== validApiKey) {
+		return res
+		.status(403)
+		.json({ message: "Invalid API key" });
+	};
+	
+	next();
+};
+
+app.use((req, res, next) => {
+	authenticateApiKey(req, res, next);
+});
+
+app.use("/api", useRouter);
 
 app.listen(3000, () => {
-	console.log('Listening on port 3000 😎')
-})
-
+	console.log('Listening on port 3000 😎');
+});
