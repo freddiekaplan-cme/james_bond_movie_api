34bcbac832a62d905498b4258677b4a2d3e0b333
Freddie CME <freddie.kaplan@edu.cmeducations.se>
2023-05-12 13:21:29 +0200

    Add working API.

---
+++ controllers/userList.js
@@ -0,0 +1,87 @@
+let userList = [
+	{
+		name: "Freddie",
+		age: 42,
+		id: 1
+	},
+	{
+		name: "Test",
+		age: 4,
+		id: 4
+	},
+	{
+		name: "Annelie",
+		age: 41,
+		id: 2
+	}
+]
+
+let newId = userList.length + 1
+
+export const getUsers = (req, res) => {
+	res.json(userList)
+}
+
+export const createUser = (req, res) => {
+	const { name, age } = req.body
+	
+	function createUserId() {
+		const findId = userList.find(id => id.id === newId)
+		
+		if (findId) {
+			newId++
+			console.log("Id found in list!")
+			return createUserId()
+		} else {
+			userList.push({
+				name: name,
+				age: age,
+				id: newId
+			})
+		}
+	}
+	createUserId()
+	
+	console.log(name, age, newId)
+
+	res.json(userList)
+}
+
+export const getOneUser = (req, res) => {
+	const userId = req.params.id
+
+	const user = userList.find(user => {
+		return user.id === Number(userId)
+	})
+
+	res.json(user)
+}
+
+export const deleteUser = (req, res) => {
+	const userId = req.params.id
+
+	userList = userList.filter(user => {
+		return user.id !== Number(userId)
+	})
+
+	res.json(userList)
+}
+
+export const editUser = (req, res) => {
+	const userId = req.params.id
+	const {age, name} = req.body
+
+	userList = userList.map(function(user) {
+		if (user.id === Number(userId)) {
+			return {
+				name,
+				age,
+				id: user.id
+			}
+		}
+
+		return user
+	})
+
+	res.json(userList)
+}
--- routes/users.js
+++ routes/users.js
@@ -1,79 +1,12 @@
 import express, { Router } from "express"
-import userList from "./userList.js"
-import { updateUserList } from "./userList.js"
+import { getUsers, createUser, getOneUser, deleteUser, editUser} from "../controllers/userList.js"
 
 const router = express.Router()
 
-let newId = userList.length + 1
-
-router.get('/users', (req, res) => {
-	res.json(userList)
-})
-
-router.post('/users', (req, res) => {
-	const { name, age }= req.body
-	
-	function createUserId() {
-		const findId = userList.find(id => id.id === newId)
-		
-		if (findId) {
-			newId++
-			console.log("Id found in list!")
-			return createUserId()
-		} else {
-			userList.push({
-				name: name,
-				age: age,
-				id: newId
-			})
-		}
-	}
-	createUserId()
-	
-	console.log(name, age, newId)
-
-	res.json(userList)
-})
-
-router.get('/users/:id', (req, res) => {
-	const userId = req.params.id
-
-	const user = userList.find(user => {
-		return user.id === Number(userId)
-	})
-
-	res.json(user)
-})
-
-router.delete('/users/:id', (req, res) => {
-	const userId = req.params.id
-
-	const updatedUserList = userList.filter(user => {
-		return user.id !== Number(userId)
-	})
-
-	updateUserList(updatedUserList);
-
-	res.json(userList)
-})
-
-router.put('/users/:id', (req, res) => {
-	const userId = req.params.id
-	const {age, name} = req.body
-
-	userList = userList.map(user => {
-		if (userList.id === userId) {
-			return {
-				name,
-				age,
-				id: user.id
-			}
-		}
-
-		return user
-	})
-
-	res.json(userList)
-})
+router.get('/users', getUsers)
+router.post('/users', createUser)
+router.get('/users/:id', getOneUser)
+router.delete('/users/:id', deleteUser)
+router.put('/users/:id', editUser)
 
 export default router
